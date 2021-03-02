d420bb66ed404b6716df8901af8eb33af44311d9
Aaron Gomes <aarongomes@Aarons-MacBook-Pro.local>
2021-03-02 15:12:14 +0530

    Version 1.0.1

--- server.js
+++ server.js
@@ -2,26 +2,18 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-
-// const socket = require("socket.io");
-// const io = socket(server);
-// const cors = require("cors");
-// app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+const path = require("path");
 
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 io.on("connection", (socket) => {
